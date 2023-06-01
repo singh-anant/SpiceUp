5b895c7e6f745f0f021c9f59e6211af10ea7e3c5
Anant-Singh1 <97743499+Anant-Singh1@users.noreply.github.com>
2023-06-01 14:01:53 +0530

    add react-icons library for cart icon

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,4 +1,6 @@
 import logo from "../../assets/logo.png";
+import { CiShoppingCart } from "react-icons/ci";
+// import cart from "../../assets/cart.png";
 const HeaderComponent = () => {
   return (
     <div className="header">
@@ -8,7 +10,10 @@ const HeaderComponent = () => {
           <li>Home</li>
           <li>About</li>
           <li>Food</li>
-          <li>Ecart</li>
+          <li className="h-cart">
+            <span className="h-count">1</span>
+            <CiShoppingCart className="h-carticon" />
+          </li>
         </ul>
       </div>
     </div>
