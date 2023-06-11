395f83aaef77c0b97cfa4dcdf63ed62c802cc8a9
Anant-Singh1 <97743499+Anant-Singh1@users.noreply.github.com>
2023-06-11 18:00:00 +0530

    Integerated payment using gpay

--- src/components/CartComponent.js
+++ src/components/CartComponent.js
@@ -5,6 +5,7 @@ import { clearCart } from "../../utils/cartSlice.js";
 import emptyCartImg from "../../assets/empty_cart.png";
 import { Link } from "react-router-dom";
 import { clearMoney } from "../../utils/amountSlice.js";
+import GooglePayButton from "@google-pay/button-react";
 const CartComponent = () => {
   const cartItems = useSelector((store) => store.cart.items);
   const totalamount = useSelector((store) => store.amount.totalprice);
@@ -42,6 +43,54 @@ const CartComponent = () => {
         <h2 className="c-total-amount">
           Total Amount-<span>₹{totalamount.toFixed(2)}</span>
         </h2>
+      </div>
+      <div className="c-checkout-wrapper">
+        <GooglePayButton
+          environment="TEST"
+          paymentRequest={{
+            apiVersion: 2,
+            apiVersionMinor: 0,
+            allowedPaymentMethods: [
+              {
+                type: "CARD",
+                parameters: {
+                  allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+                  allowedCardNetworks: ["MASTERCARD", "VISA"],
+                },
+                tokenizationSpecification: {
+                  type: "PAYMENT_GATEWAY",
+                  parameters: {
+                    gateway: "example",
+                    gatewayMerchantId: "exampleGate",
+                  },
+                },
+              },
+            ],
+            merchantInfo: {
+              merchantId: "123456733",
+              merchantName: "Demo Merchant",
+            },
+            transactionInfo: {
+              totalPriceStatus: "FINAL",
+              totalPriceLabel: "Total",
+              totalPrice: totalamount.toFixed(2),
+              currencyCode: "INR",
+              countryCode: "IN",
+            },
+            shippingAddressRequired: true,
+            callbackIntents: ["PAYMENT_AUTHORIZATION"],
+          }}
+          onLoadPaymentData={(paymentRequest) => {
+            console.log(paymentRequest);
+          }}
+          onPaymentAuthorized={(paymentData) => {
+            console.log(paymentData);
+            return { transactionState: "SUCCESS" };
+          }}
+          existingPaymentMethodRequired="false"
+          buttonColor="Yellow"
+          buttonLocale="checkout"
+        ></GooglePayButton>
       </div>
     </div>
   );
